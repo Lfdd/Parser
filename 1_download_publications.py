315df324e0686fadd4001eceb724fcc86ae154df
Vladimir98 <vladimirshitov98@gmail.com>
2021-02-26 09:58:23 +0700

    Try to parse several authors

--- 1_download_publications.py
+++ 1_download_publications.py
@@ -1,7 +1,34 @@
 from elibrary_parser.Parsers import AuthorParser
+from elibrary_parser.utils import find_common_publications
 
-AUTHOR_ID = "679517"
+author_ids = [
+    "679517",  # Марков
+    "108193",  # Пузырёв
+    "638022"  # Слепцов
+]
 
-parser = AuthorParser(author_id=AUTHOR_ID)
-parser.find_publications()
-parser.parse_publications()
+publications = []
+
+for author_id in author_ids:
+    parser = AuthorParser(author_id=author_id, data_path="C:/Users/vladi/PycharmProjects/Parser/data/")
+    # parser.find_publications()
+    parser.parse_publications()
+    parser.save_publications()
+
+    publications.append(set(parser.publications))
+
+for author_publications in publications:
+    for publication in author_publications:
+        print(publication.title)
+        print(publication.authors)
+        print("-"*20)
+        print()
+
+
+common_publications = find_common_publications(publications)
+print("Found", len(common_publications), "common publications")
+
+for publication in common_publications:
+    print(publication.title)
+    print(publication.authors)
+    print("-"*20)
