c1342c16b5ba34c99342313b0b12ec27b9447419
IvanZavaruev <78853999+IvanZavaruev@users.noreply.github.com>
2021-03-02 16:44:10 +0700

    Added year of publication

--- elibrary_parser/types.py
+++ elibrary_parser/types.py
@@ -2,21 +2,21 @@ import re
 
 
 class Publication:
-    def __init__(self, title, authors, info, link):
+    def __init__(self, title, authors, info, link, year):
         self.title = title
         self.authors = authors
         self.info = info
         self.link = link
-        self.year = None
+        self.year = year
 
     def to_csv_row(self):
-        return self.title + ';' + self.authors + ';' + self.info + ';' + self.link
+        return self.title + ';' + self.authors + ';' + self.info + ';' + self.link + ';' self.year
 
-    def get_year(self):
-        years = re.findall(r'20\d{​​2}​​|19\d{​​2}​​', self.info)
+    #def get_year(self):
+     #   years = re.findall(r'20\d{​​2}​​|19\d{​​2}​​', self.info)
 
-        if years:
-            self.year = years[0]
+      #  if years:
+       #     self.year = years[0]
 
     def __eq__(self, other):
         return (
@@ -24,7 +24,8 @@ class Publication:
                 and self.authors == other.authors
                 and self.info == other.info
                 and self.link == other.link
+                and self.year == other.year
         )
 
     def __hash__(self):
-        return hash(self.title) ^ hash(self.authors) ^ hash(self.info) ^ hash(self.link)
+        return hash(self.title) ^ hash(self.authors) ^ hash(self.info) ^ hash(self.link) ^ hash(self.year)
