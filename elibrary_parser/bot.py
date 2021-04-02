048a2ace230e410bbd99879b88343a6a9f2d26a3
Lfdd <77094352+Lfdd@users.noreply.github.com>
2021-04-02 11:06:20 +0700

    Modified to a working BOT for 1 author

--- elibrary_parser/bot.py
+++ elibrary_parser/bot.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -6,10 +7,12 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils import executor
+from elibrary_parser.Parsers import AuthorParser
+from elibrary_parser.utils import find_common_publications
 
 logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = '1790981905:AAHVTKBnNQJNEX-phY1Pf5sUZIeXHFHslog'
+API_TOKEN = os.environ['API_TOKEN']
 
 bot = Bot(token=API_TOKEN)
 
@@ -29,73 +32,83 @@ async def start(message: types.Message):
     await Form.authors_ids.set()
     await message.reply("Данный бот находит общие статьи между авторами на сайте elibrary."
                         "Id автора это последние цифры вида: authorid=*******. "
-                        "Просто введите id требуемых автора.")
+                        "Просто начните вводить id требуемых автора.")
 
 
 def get_keyboard():
     buttons = [
         types.InlineKeyboardButton(text="Добавить авторов", callback_data="keyboard1_extra"),
-        types.InlineKeyboardButton(text="Найти все публикации", callback_data="keyboard1_all"),
         types.InlineKeyboardButton(text="Задать промежуток", callback_data="keyboard1_Interval"),
-        types.InlineKeyboardButton(text="Найти все публикации ПОСЛЕ года", callback_data="keyboard1_After"),
-        types.InlineKeyboardButton(text="Найти все публикации ДО года", callback_data="keyboard1_Before")
     ]
     keyboard = types.InlineKeyboardMarkup(row_width=1)
     keyboard.add(*buttons)
     return keyboard
 
 
-# @dp.message_handler(lambda message: message.text.isdigit(), state=Form.authors_ids)
-
-
-@dp.message_handler(state=Form.authors_ids)
-async def cmd_numbers(message: types.Message):
-    await message.answer("Что вы хотите сделать?", reply_markup=get_keyboard())
+@dp.message_handler(lambda message: message.text.isdigit(), state=Form.authors_ids)
+async def authors_ids_dict(message: types.Message, state: FSMContext):
+    await Form.next()
+    async with state.proxy() as data:
+        data['authors_ids'] = {message.text: 'first_author'}
+    await message.answer(f"Что вы хотите сделать?", reply_markup=get_keyboard())
 
 
 @dp.callback_query_handler(Text(startswith='keyboard1_'))
-async def callbacks(call: types.CallbackQuery, message: types.Message, state: FSMContext):
+async def callbacks(call: types.CallbackQuery, state: FSMContext):
     action = call.data.split("_")[1]
     if action == "extra":
-        await call.message.edit_text(f'Окей, добавь еще')
-        async with Form.authors_ids as data:
-            data['authors_ids'] = message.text
-    elif action == "all":
-        Form.date_from = 1900
-        Form.date_to = 2021
-        await call.message.edit_text(f'Ищу все публикаци с 1900 года...Это много?')
+        await call.message.edit_text(f'Хорошо, добавим еще')
+        await Form.next()
     elif action == "Interval":
         await call.message.edit_text(f'Укажите промежуток от...')
-        async with Form.date_from as data:
-            data['date_to'] = message.text
-        await call.message.edit_text(f'...И до')
-        async with Form.date_from as data:
-            data['date_from'] = message.text
-    elif action == "After":
-        await call.message.edit_text(f'Поиск начиная от')
-        async with Form.date_from as data:
-            data['date_from'] = message.text
-    elif action == "Before":
-        await call.message.edit_text(f'Поиск до')
-        async with Form.date_from as data:
-            data['date_to'] = message.text
+        await Form.date_from.set()
 
     await call.answer()
 
 
+@dp.message_handler(lambda message: message.text.isdigit(), state=Form.authors_ids)
+async def authors_ids_dict(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['authors_ids']['second_author'] = message.text
+    await message.answer(f"Что вы хотите сделать?", reply_markup=get_keyboard())
+
+
+@dp.message_handler(lambda message: message.text.isdigit(), state=Form.date_from)
+async def process_date_from(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['date_from'] = message.text
+    await message.reply("...И до какого года")
+    await Form.date_to.set()
+
+
+@dp.message_handler(lambda message: message.text.isdigit(), state=Form.date_to)
+async def process_date_to(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['date_to'] = message.text
+        for data['authors_ids'] in data['authors_ids']:
+            parser = AuthorParser(
+                author_id=data['authors_ids'],
+                data_path="C://Users//SZ//PycharmProjects//Parser//data",
+                date_from=int(data['date_from']),
+                date_to=int(data['date_to'])
+            )
+            parser.find_publications()  # Загрузка HTML-файлов с публикациями
+            parser.parse_publications()  # Извлечение информации из HTML-файлов
+            parser.save_publications()  # Сохранение информации в CSV-файл
+
+            path = r"C:/Users/SZ/PycharmProjects/Parser/data/processed/" + \
+                (data['authors_ids']) + r'/publications.csv'
+
+            await bot.send_document(message.chat.id, open(path, 'rb'))
+
+
 @dp.message_handler(lambda message: not message.text.isdigit(), state=Form.date_from)
 async def process_date_from_invalid(message: types.Message):
-    """
-    If date is invalid
-    """
     return await message.reply('Пожалуйста вводите только числа')
 
 
 @dp.message_handler(lambda message: not message.text.isdigit(), state=Form.date_to)
 async def process_date_to_invalid(message: types.Message):
-    """
-    If date is invalid
-    """
     return await message.reply('Пожалуйста вводите только числа')
 
 
